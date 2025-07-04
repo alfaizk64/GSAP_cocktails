d42089fc8d746700727865859eb0d6c9a4196393
alfaiz khan <84037292+alfaizk64@users.noreply.github.com>
2025-07-04 18:25:00 +0530

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import { ScrollTrigger, SplitText } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger,SplitText)
+
+function App() {
+
+  return (
+    <>
+         
+    </>
+  )
+}
+
+export default App
